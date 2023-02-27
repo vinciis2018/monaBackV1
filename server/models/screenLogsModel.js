69b0840326b2f3192854171421a905076067efc0
vinciis2018 <vinciis2018@gmail.com>
2023-02-27 18:49:47 +0530

    Kishan

--- server/models/screenLogsModel.js
+++ server/models/screenLogsModel.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
 
+const playingDataSchema = new mongoose.Schema(
+  {
+    deviceInfo: { type: String },
+    playTime: { type: Date },
+    playVideo: { type: String },
+  }, {
+    timestamps: true,
+  }
+);
 const screenLogsSchema = new mongoose.Schema(
   {
     screen: { type: mongoose.Schema.Types.ObjectId, ref: "Screen" },
-    playingDetails: {}
+    playingDetails: [playingDataSchema]
   },
   {
     timestamps: true,
